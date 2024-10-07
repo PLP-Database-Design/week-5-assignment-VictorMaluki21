2e4e9996ae7479beae8b05f14295e897c4343a99
VictorMaluki21 <victormaluki27@gmail.com>
2024-10-07 16:25:29 +0300

    week 5 assignment

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
+const app = express();
+
+// Set up the database connection
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection failed: ', err.stack);
+    return;
+  }
+  console.log('Connected to the database.');
+});
+
+// 1.Retrieve all patients
+
+app.get('/patients', (req, res) => {
+  const query = 'SELECT patient_id, first_name, last_name, date_of_birth FROM patients';
+  db.query(query, (err, results) => {
+    if (err) {
+      res.status(500).send('Error retrieving patients');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// 2.Retrieve all providers
+
+app.get('/providers', (req, res) => {
+  const query = 'SELECT first_name, last_name, provider_specialty FROM providers';
+  db.query(query, (err, results) => {
+    if (err) {
+      res.status(500).send('Error retrieving providers');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// 3.Filter patients by first name
+
+app.get('/patients/:first_name', (req, res) => {
+  const firstName = req.params.first_name;
+  const query = 'SELECT patient_id, first_name, last_name, date_of_birth FROM patients WHERE first_name = ?';
+  db.query(query, [firstName], (err, results) => {
+    if (err) {
+      res.status(500).send('Error retrieving patients');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// 4.Retrieve all providers by their specialty
+
+app.get('/providers/specialty/:specialty', (req, res) => {
+  const specialty = req.params.specialty;
+  const query = 'SELECT first_name, last_name, provider_specialty FROM providers WHERE provider_specialty = ?';
+  db.query(query, [specialty], (err, results) => {
+    if (err) {
+      res.status(500).send('Error retrieving providers');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
